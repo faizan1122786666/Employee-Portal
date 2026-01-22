c630d2c5485e7b1139fdc9d07455761e00bd383d
M Faizan Javed <faizan9664740@gmail.com>
2026-01-22 15:20:47 +0500

    Icons changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,39 @@
+// import React from 'react'
+// import { Menu } from 'lucide-react'
+
+// export default function Header({ title, handleSlidebar }) {
+//   return (
+//     <header
+//       className="
+//         fixed top-0 right-0 h-16 bg-white z-40 flex items-center gap-4 px-6 *: border-b border-gray-200 left-0 lg:left-64">
+            
+//        {/* Icon */}
+//       <button onClick={handleSlidebar} className="lg:hidden">
+//         <Menu size={24} />
+//       </button>
+       
+//        {/* Title */}
+//       <h1 className="text-xl font-semibold text-[#2C5282]">
+//         {title}
+//       </h1>
+//     </header>
+//   )
+// }
+
+
+
 import React from 'react'
-import { Menu } from 'lucide-react'
+import { AiOutlineMenu } from 'react-icons/ai'
 
 export default function Header({ title, handleSlidebar }) {
   return (
     <header
       className="
-        fixed top-0 right-0 h-16 bg-white z-40 flex items-center gap-4 px-6 *: border-b border-gray-200 left-0 lg:left-64">
+        fixed top-0 right-0 h-16 bg-white z-40 flex items-center gap-4 px-6 border-b border-gray-200 left-0 lg:left-64">
             
        {/* Icon */}
       <button onClick={handleSlidebar} className="lg:hidden">
-        <Menu size={24} />
+        <AiOutlineMenu size={24} />
       </button>
        
        {/* Title */}
