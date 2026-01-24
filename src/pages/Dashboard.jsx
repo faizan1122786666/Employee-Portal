382efd58020375edfd3dc4c7bf1b304b4445c32e
M Faizan Javed <faizan9664740@gmail.com>
2026-01-24 17:23:40 +0500

    Charts added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react'
-import { AiOutlineUser } from 'react-icons/ai'
 import { RxCrossCircled } from "react-icons/rx";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import { SlCalender } from "react-icons/sl";
+import { Doughnut,Line } from 'react-chartjs-2';
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+ChartJS.register(ArcElement, Tooltip, Legend)
+import { LineElement, PointElement, CategoryScale, LinearScale, Title } from 'chart.js'
+ChartJS.register(
+  ArcElement,
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend
+)
 const mockdata = {
   totalemployees: 110,
   todaypresent: 95,
@@ -12,6 +26,67 @@ const mockdata = {
   todayleave: 5,
 }
 
+const chartData = {
+  labels: ['Present', 'Absent', 'On Leave'],
+  datasets: [
+    {
+      label: 'Employees',
+      data: [
+        mockdata.todaypresent,
+        mockdata.todayabsent,
+        mockdata.todayleave,
+      ],
+      backgroundColor: [
+        'rgb(71, 85, 105)',  
+        'rgb(148, 163, 184)',   
+        'rgb(203, 213, 225)',   
+      ],
+      hoverOffset: 4,
+    },
+  ],
+}
+
+const weeklyAttendanceLabels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+const weeklyAttendanceData = {
+  labels: weeklyAttendanceLabels,
+  datasets: [
+    {
+      label: 'Present Employees',
+      data: [95, 90, 85, 92, 88, 94, 90],
+      fill: false,
+      borderColor: 'rgb(71, 85, 105)',
+      backgroundColor: 'rgba(71, 85, 105, 0.2)',
+      tension: 0.3,
+      pointBackgroundColor: 'rgb(71, 85, 105)',
+      pointBorderColor: '#fff',
+      pointBorderWidth: 2,
+      pointRadius: 5,
+      pointHoverRadius: 8,
+    },
+  ],
+};
+
+
+const EmployeeTaskPerformanceDate = {
+   labels:['Completed','Pending','In Progress'],
+   datasets:[
+    {
+      label:'Tasks',
+      data:[
+        mockdata.todaypresent,
+        mockdata.todayabsent,
+        mockdata.todayabsent,
+      ],
+       backgroundColor: [
+        'rgb(71, 85, 105)',   
+        'rgb(148, 163, 184)',  
+        'rgb(203, 213, 225)',   
+      ],
+      hoverOffset:4,
+    },
+   ],
+}
 function Dashboard({ setTitle }) {
   const [data, setData] = useState(mockdata)
 
@@ -20,8 +95,9 @@ function Dashboard({ setTitle }) {
   }, [setTitle])
 
   return (
+  <>
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-5">
-      <div className="bg-white p-5 rounded-xl border-l-4 border-[#2C5284] flex items-center justify-between shadow w-full min-h-30">
+      <div className="bg-white p-5 rounded-xl border-l-4 border-[#2C5284] flex items-center justify-between shadow w-full min-h-30 hover:shadow-xl transform transition duration-300 ease-in-out">
         <div>
           <p className="text-sm sm:text-base text-[#2C5284]">
             Total Employees
@@ -36,7 +112,7 @@ function Dashboard({ setTitle }) {
         </div>
       </div>
 
-        <div className="bg-white p-5 rounded-xl border-l-4 border-[#2C5284] flex items-center justify-between shadow w-full min-h-30">
+         <div className="bg-white p-5 rounded-xl border-l-4 border-[#2C5284] flex items-center justify-between shadow w-full min-h-30 hover:shadow-xl transform transition duration-300 ease-in-out">
           <div>
           <p className="text-sm sm:text-base text-[#2C5284]">
             Present Today
@@ -50,7 +126,7 @@ function Dashboard({ setTitle }) {
         </div>
       </div>
 
-        <div className="bg-white p-5 rounded-xl border-l-4 border-[#2C5284] flex items-center justify-between shadow w-full min-h-30">
+        <div className="bg-white p-5 rounded-xl border-l-4 border-[#2C5284] flex items-center justify-between shadow w-full min-h-30 hover:shadow-xl transform transition duration-300 ease-in-out">
           <div>
           <p className="text-sm sm:text-base text-[#2C5284]">
             Absent Today
@@ -64,7 +140,7 @@ function Dashboard({ setTitle }) {
         </div>
       </div>
 
-       <div className="bg-white p-5 rounded-xl border-l-4 border-[#2C5284] flex items-center justify-between shadow w-full min-h-30">
+       <div className="bg-white p-5 rounded-xl border-l-4 border-[#2C5284] flex items-center justify-between shadow w-full min-h-30 hover:shadow-xl transform transition duration-300 ease-in-out">
           <div>
           <p className="text-sm sm:text-base text-[#2C5284]">
             onLeave
@@ -79,10 +155,76 @@ function Dashboard({ setTitle }) {
       </div>
 
     </div>
+
+
+   <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10">
+   <div className="bg-white p-6 rounded-xl shadow hover:shadow-xl transform transition duration-300 ease-in-out">
+    <h2 className="text-lg font-bold text-[#2C5284] mb-4">
+      Today Attendance Overview
+    </h2>
+
+    <div className="w-full flex justify-center">
+      <div className="w-64 sm:w-72 md:w-64">
+        <Doughnut data={chartData} />
+       </div>
+     </div>
+    </div>
+
+    <div className="bg-white p-6 rounded-xl shadow hover:shadow-xl transform transition  duration-300 ease-in-out">
+    <h2 className="text-lg font-bold text-[#2C5284] mb-4">
+      Today Employee Task Performance
+    </h2>
+
+    <div className="w-full flex justify-center">
+      <div className="w-64 sm:w-72 md:w-64">
+        <Doughnut data={EmployeeTaskPerformanceDate} />
+      </div>
+    </div>
+    </div>
+  </div>
+
+  <div className="grid grid-cols-1 mt-10">
+    <div  className="bg-white p-6 rounded-xl shadow hover:shadow-xl transform transition duration-300 ease-in-out">
+      <h2  className="text-lg font-bold text-[#2C5284] mb-4">
+        Weekly Attendance Overview</h2>
+      <div className="w-full flex justify-center">
+        <div className="w-full md:w-3/4 lg:w-2/3">
+           <Line data={weeklyAttendanceData} options={{maintainAspectRatio:false}}/>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </>
   )
 }
 
 export default Dashboard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // import { useState, useEffect } from 'react';
