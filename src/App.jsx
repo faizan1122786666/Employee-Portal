f80384dad36bdfbbd5b143db94f2919925e08ee2
M Faizan Javed <faizan9664740@gmail.com>
2026-02-10 20:24:30 +0500

    All is Completed

--- src/App.jsx
+++ src/App.jsx
@@ -116,15 +116,151 @@
 
 
 
+// import { useEffect, useState } from 'react'
+// import { Routes, Route, Navigate } from 'react-router-dom'
+// import Slidebar from './components/layout/Slidebar'
+// import Header from './components/layout/Header'
+// import Login from './pages/Auth/Login.jsx'
+// import { AuthContextProvider} from './context'
+// // import {AdminDashboard,UserDashboard} from './pages/dashboard/index'
+// import {AdminDashboard,UserDashboard} from './pages/dashboard/index'
+// import {AdminAttendance,UserAttendance} from './pages/attendance/index'
+// import { AdminLeave,UserLeave } from './pages/leave/index'
+// // import EmployeeAttendanceModal from './pages/Attendance/EmployeeAttendanceModal'
+
+// function App () {
+//   const [isOpen, setIsOpen] = useState(false)
+//   const [title, setTitle] = useState('Dashboard')
+//   const [user, setUser] = useState(null)
+//   const [darkMode, setDarkMode] = useState(false)
+  
+//   const handleSlidebar = () => {
+//     setIsOpen(!isOpen)
+//   }
+
+//   useEffect (() => {
+//     const stored = localStorage.getItem('user')
+//     if(stored)
+//     {
+//       // eslint-disable-next-line react-hooks/set-state-in-effect
+//       setUser(JSON.parse(stored))
+//     }
+//   },[]) 
+
+//   const handleLogin = (userData) => {
+//   localStorage.setItem('user',JSON.stringify(userData))
+//   setUser(userData)
+//   }
+  
+//   const handleLogout = () => {
+//     localStorage.removeItem('user')
+//     setUser(null)
+//   }
+
+//   const toggleDarkMode = () => {
+//     setDarkMode(prev => !prev);
+//   }
+
+
+//   useEffect(() => {
+//     const html  = document.documentElement
+//     darkMode ? html.classList.add('dark')
+//     : html.classList.remove('dark')
+//   }, [darkMode])
+  
+
+
+//   return (
+   
+//     <AuthContextProvider value={{user, handleLogin, handleLogout}}>
+//       <div className="flex h-screen overflow-hidden">
+//       { user ? (
+//         <>
+//        <Slidebar 
+//          isOpen={isOpen} 
+//          handleSlidebar={handleSlidebar} 
+//          userEmail={user.email || '' }
+//          userName={user.name || ''}
+//          onLogout={handleLogout}/>
+
+//            <div className="flex-1 flex flex-col">
+
+//    <Header title={title} handleSlidebar={handleSlidebar} 
+//    darkMode={darkMode}  toggleDarkMode={toggleDarkMode}
+//    />
+
+//    {/* <main className="pt-16 px-6 bg-gray-50 flex-1 overflow-y-auto"> */}
+//    <main className="pt-16 px-6 bg-gray-50 dark:bg-gray-900 flex-1 overflow-y-auto">
+//        <Routes>
+//            <Route path="/" element={ 
+//             user.role === 'admin' ? (
+//                <AdminDashboard setTitle={setTitle} darkMode={darkMode} />
+//             ) : (
+//               <UserDashboard setTitle={setTitle} darkMode={darkMode}/>
+//             )
+//             } 
+//             />
+//            <Route path="/attendance" element={
+//             user.role === 'admin' ? (
+
+//             <AdminAttendance setTitle={setTitle}  />)
+//             : (
+//             <UserAttendance setTitle={setTitle} />
+//             )
+//             } />
+           
+//            {/* Employee Detail Route - Only for Admin
+//            {user.role === 'admin' && ( */}
+   
+   
+           
+//            <Route path="/leave" element={
+//             user.role === 'admin' ? (
+//               <AdminLeave setTitle={setTitle} />
+//             ) : (
+//               <UserLeave setTitle={setTitle} />
+//             )
+//            } />
+           
+//            {/* Redirect to dashboard if already logged in */}
+//            <Route path='/login' element={<Navigate to="/" replace/>} />
+           
+//            {/* Catch all - redirect to dashboard */}
+//            <Route path='*' element={<Navigate to="/" replace />} />
+//        </Routes>
+//    </main>
+//       </div>
+//        </> ) : (
+//         <Login />
+//         )}
+
+//         </div>
+//     </AuthContextProvider>
+   
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+
+
 import { useEffect, useState } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Slidebar from './components/layout/Slidebar'
 import Header from './components/layout/Header'
 import Login from './pages/Auth/Login.jsx'
 import { AuthContextProvider} from './context'
-import {AdminDashboard,UserDashboard} from './pages/dashboard/index'
-import {AdminAttendance,UserAttendance} from './pages/attendance/index'
-import { AdminLeave,UserLeave } from './pages/leave/index'
+import {AdminDashboard,UserDashboard} from './pages/Dashboard/index'
+import {AdminAttendance,UserAttendance} from './pages/Attendance/index'
+import { AdminLeave,UserLeave } from './pages/Leave/index'
 // import EmployeeAttendanceModal from './pages/Attendance/EmployeeAttendanceModal'
 
 function App () {
@@ -240,6 +376,3 @@ function App () {
 }
 
 export default App
-
-
-
